8fefe84e946e9e81568ddb418d5da5450c682d69
Phan Cao Thanh Vinh <henry.phan2601@gmail.com>
2026-03-25 22:17:28 -0600

    add-auth-context

---
+++ components/AuthContext.tsx
@@ -0,0 +1,11 @@
+import { createContext, useState } from 'react';
+export const AuthContext = createContext<any>(null);
+export const AuthProvider = ({ children }: any) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedUp, setIsLoggedUp] = useState(false);
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, isLoggedUp, setIsLoggedIn, setIsLoggedUp }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
