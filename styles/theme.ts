97e017cb7aa958f4ad8215d47b65cbe6820c7474
Phan Cao Thanh Vinh <henry.phan2601@gmail.com>
2026-03-25 22:56:19 -0600

    add style for Knowly

---
+++ styles/theme.ts
@@ -0,0 +1,34 @@
+export const logo = {
+  text: "#4647D3",
+}
+export const font = {
+  fontFamily: "Inter",
+}
+export const lightTheme = {
+  background: "#FFFFFF",
+  cardBackground: "#ECF1FF",
+  card: "#B0C4FF",
+  text: "#000000",
+  incompleteCheckbox: '#B0C4FF',
+  completedCheckbox: '#006947',
+  errorText: '#FF0303',
+}
+export const darkTheme = {
+  background: "#0F172A",
+  cardBackground: "#1E293B",
+  card: "#94A4D6",
+  text: "#FFFFFF",
+  incompleteCheckbox: "#B0C4FF",
+  completedCheckbox: "#00DD96",
+  errorText: '#FF0303',
+}
+export const taskProgress = {
+  notStarted: "#D9D9D9",
+  inProgress: "#B0C4FF",
+  done: "#1AFE35",
+}
+export const button = {
+  main: "#4647D3",
+  delete: "#FF0303",
+  cancel: "#B0C4FF",
+}
