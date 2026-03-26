a380255d47b1738216f9791bd99b773d291f255f
Phan Cao Thanh Vinh <henry.phan2601@gmail.com>
2026-03-25 22:07:52 -0600

    add theme-component

---
+++ components/ThemeContext.tsx
@@ -0,0 +1,18 @@
+import React, { createContext, useState, ReactNode } from 'react';
+type ThemeContextType = {
+  isDark: boolean;
+  toggleTheme: () => void
+}
+type ProviderProps = {
+  children: ReactNode;
+}
+export const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+export const ThemeProvider = ({ children }: ProviderProps) => {
+  const [isDark, setDark] = useState<boolean>(false);
+  const toggleTheme = () => setDark(prev => !prev);
+  return (
+    <ThemeContext.Provider value={{ isDark, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
