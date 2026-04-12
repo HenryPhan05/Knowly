c0694868cd10786cbea3a485090f30b8441bb213
Phan Cao Thanh Vinh <henry.phan2601@gmail.com>
2026-04-12 15:27:56 -0600

    updated useTheme

--- hook/useTheme.ts
+++ hook/useTheme.ts
@@ -1,7 +1,11 @@
-import { useContext } from "react";
 import { ThemeContext } from "@/components/ThemeContext";
-import { lightTheme, darkTheme } from "@/styles/theme";
+import { darkTheme, lightTheme } from "@/styles/theme";
+import { useContext } from "react";
+
+
 export const useTheme = () => {
-  const { isDark } = useContext(ThemeContext)!;
+
+  const { isDark, toggleTheme } = useContext(ThemeContext)!;
+
   return isDark ? darkTheme : lightTheme;
 }
