ac39521d92c91f0cb78cb7da6308ceb671346a2b
Thanh Vinh <henry.phan2601@gmail.com>
2026-04-12 15:39:59 -0600

    Refactor layout to use auth and theme hooks
    Refactored layout to use authentication and theme hooks, replacing the previous layout structure with a redirect based on session status.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,18 @@
+import { useAuth } from "@/hook/useAuth";
+import { useTheme } from "@react-navigation/native";
+import { Redirect } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
+export default function Index() {
+  const { session, isLoading } = useAuth();
+  const theme = useTheme();
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: "center", backgroundColor: theme.colors.background }}>
+        <ActivityIndicator size="large" color={theme.colors.primary} />
+      </View>
+    );
+  }
 
-import { AuthProvider } from "@/components/AuthContext";
-import { ThemeProvider } from "@/components/ThemeContext";
-import { Slot } from "expo-router";
+  return <Redirect href={session ? "/(tabs)/home" : "/login"} />;
 
-export default function RootLayout() {
-  return (
-    <AuthProvider>
-      <ThemeProvider>
-        <Slot />
-      </ThemeProvider>
-    </AuthProvider>
-  );
 }
