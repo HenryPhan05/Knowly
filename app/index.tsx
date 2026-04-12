d96522d1b3a94bd2d7d4b0116e27c1cba7ba9d44
Thanh Vinh <henry.phan2601@gmail.com>
2026-04-12 15:40:56 -0600

    Implement loading state and session redirect

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,18 @@
-import { Redirect, router } from "expo-router";
-
-
+import { useAuth } from "@/hook/useAuth";
+import { useTheme } from "@react-navigation/native";
+import { Redirect } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
 export default function Index() {
-  return <Redirect href="/signin" />;
+  const { session, isLoading } = useAuth();
+  const theme = useTheme();
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: "center", backgroundColor: theme.colors.background }}>
+        <ActivityIndicator size="large" color={theme.colors.primary} />
+      </View>
+    );
+  }
+
+  return <Redirect href={session ? "/(tabs)/home" : "/login"} />;
+
 }
